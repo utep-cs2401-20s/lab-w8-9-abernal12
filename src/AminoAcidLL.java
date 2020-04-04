dcb31ae84e5a88050a858bc483c3c5461f0325ef
abernal12 <53953393+abernal12@users.noreply.github.com>
2020-04-03 20:21:46 -0600

    finished sort method and edited and organized comments.

--- src/AminoAcidLL.java
+++ src/AminoAcidLL.java
@@ -1,10 +1,10 @@
 import jdk.nashorn.internal.objects.NativeString;
 
 class AminoAcidLL{
-  private char aminoAcid; // the character representing the Amino Acid stored in this element
-  private String[] codons; // the codons that represent this Amino Acid
-  private int[] counts; // the count of the codon usage... codons.length will be equal to counts.length
-  private AminoAcidLL next; // pointer to next node // the next element in the linked list, had the value NULL when the current element is the last
+  private char aminoAcid; // ...........................................................................................the character representing the Amino Acid stored in this element
+  private String[] codons; // ..........................................................................................the codons that represent this Amino Acid
+  private int[] counts; // .............................................................................................the count of the codon usage... codons.length will be equal to counts.length
+  private AminoAcidLL next; // .........................................................................................pointer to next node // the next element in the linked list, had the value NULL when the current element is the last
 
   AminoAcidLL(){
 
@@ -16,8 +16,8 @@ class AminoAcidLL{
    * pair and increments the codon counter for that codon.
    * NOTE: Does not check for repeats!! */
   AminoAcidLL(String inCodon){
-      aminoAcid = AminoAcidResources.getAminoAcidFromCodon(inCodon); // looks at the AminoAcidResources and gets the amino acid from the codons given
-      codons = AminoAcidResources.getCodonListForAminoAcid(aminoAcid); // looks at the AminoAcidResources and gets the codons list from the amino acids given
+      aminoAcid = AminoAcidResources.getAminoAcidFromCodon(inCodon); // ................................................looks at the AminoAcidResources and gets the amino acid from the codons given
+      codons = AminoAcidResources.getCodonListForAminoAcid(aminoAcid); // ..............................................looks at the AminoAcidResources and gets the codons list from the amino acids given
       incrCodons(inCodon); //
       next = null;
   }
@@ -42,13 +42,13 @@ class AminoAcidLL{
    */
   private void addCodon(String inCodon){
       // base case
-    if(AminoAcidResources.getAminoAcidFromCodon(inCodon) == aminoAcid){ // if this node has this codon
-      incrCodons(inCodon); // increments every time the node has the codon
+    if(AminoAcidResources.getAminoAcidFromCodon(inCodon) == aminoAcid){ // .............................................if this node has this codon
+      incrCodons(inCodon); // ..........................................................................................increments every time the node has the codon
     }else if(next == null) {
       next.addCodon(inCodon);
 
     }else{
-      next = new AminoAcidLL(inCodon); // if the node does not contain the codon, then start again with the next node
+      next = new AminoAcidLL(inCodon); // ..............................................................................if the node does not contain the codon, then start again with the next node
     }
   }
 
@@ -100,13 +100,13 @@ class AminoAcidLL{
     }
     int diff = 0;
     if(inList == null) {
-      diff += totalCount(); // gets the count for the total list
+      diff += totalCount(); // .........................................................................................gets the count for the total list
     }
-    if(next != null){ // checks to see if it has a pointer to another node and if so compare
+    if(next != null){ // ...............................................................................................checks to see if it has a pointer to another node and if so compare
       diff += next.aminoAcidCompare(inList.next);
     }
-    else if(aminoAcid == inList.aminoAcid) { // total count of aminoAcid and totalCount of inList
-      diff = Math.abs(totalCount() - inList.totalCount()); // to get the
+    else if(aminoAcid == inList.aminoAcid) { // ........................................................................total count of aminoAcid and totalCount of inList
+      diff = Math.abs(totalCount() - inList.totalCount()); // ..........................................................to get the
       if (next != null) {
         diff += next.aminoAcidCompare(inList.next);
       }
@@ -115,10 +115,10 @@ class AminoAcidLL{
       }
     }else if (next != null && aminoAcid < inList.aminoAcid) {
       diff += totalCount();
-      if (next != null) { // if the list is not null, class list
+      if (next != null) { // ...........................................................................................if the list is not null, class list
         diff += next.aminoAcidCompare(inList.next);
       }
-    }else if(next == null || aminoAcid > inList.aminoAcid){ // class we recieved
+    }else if (next == null || aminoAcid > inList.aminoAcid){ // ........................................................class we recieved
       diff += inList.totalCount();
       if(inList.next != null){
         diff += aminoAcidCompare(inList.next);
@@ -140,13 +140,13 @@ class AminoAcidLL{
     }
     int diff = 0;
     if(inList == null) {
-      diff += totalCount(); // gets the count for the total list
+      diff += totalCount(); // .........................................................................................gets the count for the total list
     }
-    if(next != null){ // checks to see if it has a pointer to another node and if so compare
+    if(next != null){ // ...............................................................................................checks to see if it has a pointer to another node and if so compare
       diff += next.codonCompare(inList.next);
     }
-    else if(aminoAcid == inList.aminoAcid) { // total count of aminoAcid and totalCount of inList
-      diff = Math.abs(totalCount() - inList.totalCount()); // to get the
+    else if(aminoAcid == inList.aminoAcid) { // ........................................................................total count of aminoAcid and totalCount of inList
+      diff = Math.abs(totalCount() - inList.totalCount()); // ..........................................................to get the
       if (next != null) {
         diff += next.codonCompare(inList.next);
       }
@@ -155,10 +155,10 @@ class AminoAcidLL{
       }
     }else if (next != null && aminoAcid < inList.aminoAcid) {
       diff += totalCount();
-      if (next != null) { // if the list is not null, class list
+      if (next != null) { // ...........................................................................................if the list is not null, class list
         diff += next.codonCompare(inList.next);
       }
-    }else if(next == null || aminoAcid > inList.aminoAcid){ // class we recieved
+    }else if(next == null || aminoAcid > inList.aminoAcid){ // .........................................................class we recieved
       diff += inList.totalCount();
       if(inList.next != null){
         diff += codonCompare(inList.next);
@@ -174,19 +174,30 @@ class AminoAcidLL{
   //
   public char[] aminoAcidList(){
     if(next == null){
-      return new char[]{};
+      return new char[]{aminoAcid};
     }
     char[] a = next.aminoAcidList();
-    char[] len = new char[a.length + 1]; // creating a new array with 1 size bigger than the other one
-
-    return a;
+    char[] len = new char[a.length + 1]; // ............................................................................creating a new array with 1 size bigger than the other one
+      for(int i = 1; i < len.length; i++){
+          len[i + 1] = a[i - 1];
+      }
+      len[0] = aminoAcid;
+    return len;
   }
 
   /********************************************************************************************/
   /* Recursively returns the total counts of amino acids in the order that they are in in the linked list. */
   public int[] aminoAcidCounts(){
-    
-    return new int[]{};
+      if(next == null){
+          return new int[]{totalCount()};
+      }
+      int[] a = next.aminoAcidCounts();
+      int[] len = new int[a.length + 1]; // ............................................................................creating a new array with 1 size bigger than the other one
+      for(int i = 1; i < len.length; i++){
+          len[i + 1] = a[i - 1];
+      }
+      len[0] = totalCount();
+      return len;
   }
 
 
@@ -204,10 +215,10 @@ class AminoAcidLL{
   /* Static method for generating a linked list from an RNA sequence */
   // with the list created, you will have to use another method
   public static AminoAcidLL createFromRNASequence(String inSequence){
-    AminoAcidLL list = null; // created a linked list from the AminoAcidLL
+    AminoAcidLL list = null; // ........................................................................................created a linked list from the AminoAcidLL
     if(inSequence.length() >= 3 && AminoAcidResources.getAminoAcidFromCodon(inSequence.substring(0,2)) != '*'){
       list = new AminoAcidLL(inSequence.substring(0,3));
-      for(int i = 3; i < inSequence.length() - 2; i+=3){ // looks through the RNA string, start at index 3
+      for(int i = 3; i < inSequence.length() - 2; i+=3){ // ............................................................looks through the RNA string, start at index 3
         if (AminoAcidResources.getAminoAcidFromCodon(inSequence.substring(i, i + 3)) != '*') { //
           list.addCodon(inSequence.substring(i,i+3));
         }else{
@@ -222,15 +233,25 @@ class AminoAcidLL{
   /********************************************************************************************/
   /* sorts a list by amino acid character*/
   public static AminoAcidLL sort(AminoAcidLL inList) {
-    AminoAcidLL listSort = inList;
-    for(listSort = inList; listSort != null; listSort = listSort.next){
-
-      for(listSort node2 = node1; node2!= null; node2 = node2.next){
-        if(min.value > node2.value){
-          min = node2;
+    AminoAcidLL listStart = inList; // .................................................................................setting the starting position for the LL
+    while(listStart != null){ // .......................................................................................checks while the starting position is not equal to null then it....
+      AminoAcidLL Max = listStart; // ..................................................................................set the max to get the largest character
+      for(AminoAcidLL listStart1 = listStart; listStart1.next != null; listStart1 = listStart1.next){ //
+        if(Max.aminoAcid < listStart1.aminoAcid){ // ...................................................................if the current max is less than the next node
+          Max = listStart1; //  ........................................................................................then set the max equal to the next node
         }
       }
+      AminoAcidLL listStart1 = inList;
+      while(listStart1.next != Max){ // ................................................................................while the next node is not equal to a node larger than max
+        listStart1 = listStart.next; // ................................................................................then set the current node to the next
+      }
+      if(listStart == Max){ // .........................................................................................is the listStart node is the largest
+        listStart = listStart.next; // .................................................................................set the node to the next node
+      }
+      listStart1.next = Max.next; // ...................................................................................if the node next to the node at listStart1 is equal to the next max
+      Max.next = inList; // ............................................................................................set the next maz to the inList
+      inList = Max;
     }
-    return listSort;
+    return inList;
   }
 }
